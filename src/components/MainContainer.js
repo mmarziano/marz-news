8a72c7935e077860f5a91581facc6e3ea779af3c
Monica Marziano <mmarziano1@gmail.com>
2020-01-29 17:59:55 -0500

    search page styling

--- src/components/MainContainer.js
+++ src/components/MainContainer.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import search from '../assets/images/searchresults.jpg';
 import MainHeadline from './MainHeadline'
 import HeroImage from './HeroImage'
 import Navbar from './Navbar'
@@ -8,13 +9,17 @@ class MainContainer extends React.Component {
 
     renderResults = () => {
         return this.props.searchResults.searchArticles.map(result => 
+            
             <div className="column">
                 <figure>
                     <div className="container-article">
                         <li className="card-article">
                             <img src={result.urlToImage} alt={result.title} />
+                            <p id="author">{result.author}</p>
+                            <p id="article-date"> {result.publishedAt}</p>
+                            <a href={result.url} target="_blank"><i className="fa fa-external-link" aria-hidden="true"></i> Link to Article</a>
                             <div className='article-overlay'>
-                                <h3 classname='headline'>{result.title}</h3>
+                                <a href={result.url} target="_blank"><h3 classname='headline'>{result.title}</h3></a>
                             </div>
                         </li>
                     </div>
@@ -28,6 +33,7 @@ class MainContainer extends React.Component {
         if (this.props.searchResults.searchArticles !== null ) {
             return( 
                     <div className="article-grid">
+                        <img src={ search } alt="search results" className="header" />
                         <ul>
                             {this.renderResults()}
                   
